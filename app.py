7f750a70db07ac0f2c177cfe095947150b90b794
Moi <246282024+DOTCOM667@users.noreply.github.com>
2026-02-11 03:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ EXTERNAL_LOG_FILE = "messages_realtime.jsonl"
 DB_CONFIG = {
     'host': os.environ.get('MYSQL_HOST'),
     'user': os.environ.get('MYSQL_USER'),
-    'password': os.environ.get('MYSQL_PWD'),
-    'database': os.environ.get('MYSQL_DB'),
+    'password': os.environ.get('MYSQL_PASSWORD'),
+    'database': os.environ.get('MYSQL_DATABASE'),
     'raise_on_warnings': True
 }
 
